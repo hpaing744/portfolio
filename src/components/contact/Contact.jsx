96ecd2cd549979264d7966daf9db3b64e9b9fcbb
hpaing744 <hpaing744@gmail.com>
2023-01-07 19:42:27 +0630

    updated contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
               <h3 className="contact__card-title">
                 Email
               </h3>
-              <span className="contact__card-data">user@gmail.com</span>
+              <span className="contact__card-data">hpaing744@gmail.com</span>
 
               <a href="mailto:examplemail@gmail.com.com" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
             </div>
@@ -44,7 +44,7 @@ const Contact = () => {
               <h3 className="contact__card-title">
                 Whatsapp
               </h3>
-              <span className="contact__card-data">999-888-777</span>
+              <span className="contact__card-data">0995428740</span>
 
               <a href="https://api.whatsapp.com/send?phone=09542874440&text=Hello,more information!" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
             </div>
@@ -55,7 +55,7 @@ const Contact = () => {
               <h3 className="contact__card-title">
                 Messenger
               </h3>
-              <span className="contact__card-data">user.fb123</span>
+              <span className="contact__card-data">Htet Paing</span>
 
               <a href="https://m.me/crypticalcoder" className="contact__button">Write me <i className="bx bx-right-arrow-alt contact__button-icon"></i></a>
             </div>
